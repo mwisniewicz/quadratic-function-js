e5912a8640fff34542194b993e8460d49e3e65a4
mwisniewicz <m.wisniewicz@gmail.com>
2017-06-26 20:43:19 +0200

    Add if statements, flexbox and media queries

--- js/main.js
+++ js/main.js
@@ -5,16 +5,27 @@ function mainFunction() {
 	var first_value = parseFloat(document.getElementById("first_value").value);
 	var second_value = parseFloat(document.getElementById("second_value").value);
 	var third_value = parseFloat(document.getElementById("third_value").value);
-	var delta = deltaFunction(first_value,second_value,third_value);
-	var roots_results = roots(first_value,second_value,delta);
+	if (isNaN(first_value) || isNaN(second_value) || isNaN(third_value)) {
+		alert("Give the numbers!");
+	}
 
-    document.getElementById("result").innerHTML = "FUNKCJA POSTACI: " + 
-    first_value + "x^2 + " + second_value + "x + " + third_value + "<br>" +
-    "Delta: " + delta + "<br>" +
-    "Pierwiastki: " + "x1 = " + roots_results[0] + ", x2 = " + roots_results[1];
-    document.getElementById("first_value").value = '';
-    document.getElementById("second_value").value = '';
-    document.getElementById("third_value").value = '';
+	else {
+		var delta = deltaFunction(first_value,second_value,third_value);
+		if (delta < 0){
+			alert("Delta is less than Zero!")
+		}
+
+		else if (delta > 0) {
+			var roots_results = roots(first_value,second_value,delta);
+		    document.getElementById("result").innerHTML = "<h3>Function:</h3><span>" + 
+		    first_value + "x^2 + " + second_value + "x + " + third_value + "</span>" +
+		    "<p><strong>Delta:</strong> " + delta + "</p>" +
+		    "<p><strong>Roots:</strong> " + "x1 = " + roots_results[0] + ", x2 = " + roots_results[1] + "</p>";
+		    document.getElementById("first_value").value = '';
+		    document.getElementById("second_value").value = '';
+		    document.getElementById("third_value").value = '';
+			}
+		}
 }
 
 function deltaFunction(a,b,c) {
